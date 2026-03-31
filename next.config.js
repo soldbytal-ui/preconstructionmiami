8a1ece9d134c09ca14259c1a7d7873e13e910ad3
soldbytal-ui <soldbytal@gmail.com>
2026-03-31 09:52:05 -0400

    Add deck.gl/mapbox deps and dark theme config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,19 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
+  transpilePackages: ['deck.gl', '@deck.gl/core', '@deck.gl/layers', '@deck.gl/mapbox', '@deck.gl/react'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**.supabase.co' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
+  },
+  async redirects() {
+    return [
+      { source: '/pre-construction', destination: '/new-condos', permanent: true },
+      { source: '/pre-construction/:slug', destination: '/properties/:slug', permanent: true },
+      { source: '/contact', destination: '/contact-us', permanent: true },
+    ];
   },
 };
 
